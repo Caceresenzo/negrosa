37ed81fd0aaec93d0aad866f5ea88d7cd97a2c9b
Caceresenzo <caceresenzo1502@gmail.com>
2021-02-24 17:03:09 +0100

    chore(app): Updated label and title

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Negro S.A.',
       darkTheme: appTheme(),
       themeMode: ThemeMode.dark,
       localizationsDelegates: [
