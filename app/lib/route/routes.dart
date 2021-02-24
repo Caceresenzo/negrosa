e25f01ebdd2687860dbedeac443fcc1109211636
Caceresenzo <caceresenzo1502@gmail.com>
2021-02-24 17:05:50 +0100

    chore(app): Removed unused home route

--- app/lib/route/routes.dart
+++ app/lib/route/routes.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:negrosa/screens/home/homescreen.dart';
 import 'package:negrosa/screens/references/referencesscreen.dart';
 import 'package:negrosa/screens/settings/settingsscreen.dart';
 import 'package:negrosa/screens/splash/splashscreen.dart';
@@ -7,7 +6,6 @@ import 'package:negrosa/screens/splash/splashscreen.dart';
 Map<String, WidgetBuilder> appRoutes() {
   return {
     '/splash': (context) => SplashScreen(),
-    '/': (context) => HomeScreen(),
     '/references': (context) => ReferencesScreen(),
     '/settings': (context) => SettingsScreen(),
   };
